eeb3a88040bf6aa35701c955562f562024a7535f
AcidusCode <39017486+AcidusCode@users.noreply.github.com>
2025-08-29 10:39:54 -0500

    Improving SEO

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -66,6 +66,8 @@ function setHamburgerIcon() {
       d="M4 6l16 0"></path><path d="M4 12l16 0"></path><path d="M4 18l16 0"
     ></path></svg
   >`;
+
+  btn.setAttribute('aria-label', 'Cerrar menú');
 }
 
 function closeMenu() {
